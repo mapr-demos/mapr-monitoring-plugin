8ea2445e271822e488c8192e3fc08c95369d5dc4
Vlad Storona <vstorona@cybervisiontech.com>
2018-03-28 19:02:23 +0300

    Updated dependencies, added transformer to telegraf format, removed unused code

--- src/main/java/com/mapr/monitoring/config/MonitoringPluginConfig.java
+++ src/main/java/com/mapr/monitoring/config/MonitoringPluginConfig.java
@@ -3,15 +3,14 @@ package com.mapr.monitoring.config;
 import com.mapr.monitoring.client.KafkaClient;
 import com.mapr.monitoring.client.TelegrafClient;
 import com.mapr.monitoring.properties.KafkaClientProperties;
-import com.mapr.streams.impl.admin.MarlinAdminImpl;
 import feign.Feign;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,10 +19,9 @@ public class MonitoringPluginConfig {
     @Value("${telegraph.path}")
     private String telegraphUrl;
 
-    @SneakyThrows
-    public MarlinAdminImpl adminClient(){
-        org.apache.hadoop.conf.Configuration configuration = new org.apache.hadoop.conf.Configuration();
-        return new MarlinAdminImpl(configuration);
+    @Bean
+    public AdminClient adminClient() {
+        return AdminClient.create(new HashMap<>());
     }
 
     @Bean
@@ -34,8 +32,6 @@ public class MonitoringPluginConfig {
     @Bean
     public TelegrafClient telegrafClient() {
         return Feign.builder()
-                .decoder(new JacksonDecoder())
-                .encoder(new JacksonEncoder())
                 .target(TelegrafClient.class, telegraphUrl);
     }
 }
--- src/main/java/com/mapr/monitoring/service/StreamingService.java
+++ src/main/java/com/mapr/monitoring/service/StreamingService.java
@@ -4,15 +4,16 @@ import com.google.common.collect.Lists;
 import com.mapr.monitoring.client.KafkaClient;
 import com.mapr.monitoring.client.TelegrafClient;
 import com.mapr.monitoring.properties.MonitoringPluginProperties;
-import com.mapr.streams.impl.admin.MarlinAdminImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.stereotype.Service;
 import reactor.core.scheduler.Schedulers;
 
 import javax.annotation.PostConstruct;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,7 +21,7 @@ import java.util.stream.Stream;
 @Service
 @RequiredArgsConstructor
 public class StreamingService {
-    private final MarlinAdminImpl adminClient;
+    private final AdminClient adminClient;
     private final KafkaClient kafkaClient;
     private final TelegrafClient telegrafClient;
     private final MonitoringPluginProperties properties;
@@ -36,6 +37,7 @@ public class StreamingService {
 
         kafkaClient.subscribeOnMessages(convertedTopics, String.class)
                 .map(this::convertToTelegrafFormat)
+                .log()
                 .doOnNext(telegrafClient::writeMetric)
                 .doOnError(t -> log.error("Error while sending metric to telegraf", t))
                 .subscribeOn(Schedulers.single())
@@ -45,20 +47,42 @@ public class StreamingService {
     private Stream<String> convertToStringStream(String stream) {
         return getAllTopicsForStream(stream)
                 .stream()
-                .map(topic -> convertHostStreamTopic(properties.getClusterHost(), stream, topic));
+                .map(topic -> convertHostStreamTopic(stream, topic));
     }
 
     @SneakyThrows
     private Set<String> getAllTopicsForStream(String streamName) {
-        return adminClient.listTopics(streamName).keySet();
+        return adminClient.listTopics(streamName).names().get();
     }
 
-    private String convertHostStreamTopic(String host, String stream, String topic) {
-        return String.format("%s:%s_%s", stream, host, topic);
+    private String convertHostStreamTopic(String stream, String topic) {
+        return String.format("%s:%s", stream, topic);
     }
 
-    //TODO: write method that converts from kafka format to telegraf format
     private String convertToTelegrafFormat(String initial) {
-        return initial;
+
+        String withoutPut = normalize(initial);
+        String tags = getTags(initial);
+        withoutPut = removeTags(withoutPut);
+
+        String[] split = withoutPut.split(" ");
+
+        String timestamp = TimeUnit.NANOSECONDS.convert(Long.parseLong(split[1]), TimeUnit.MILLISECONDS) + "";
+
+        return String.format("%s,%s value=%s %s", split[0], tags, split[2], timestamp);
+    }
+
+    private static String normalize(String str) {
+        String regex = "\\s*\\bput\\b\\s*";
+        return str.replaceAll(regex, "");
+    }
+
+    private static String getTags(String str) {
+        return str.substring(str.indexOf("fqdn"))
+                .replaceAll("\\s+", ",");
+    }
+
+    private static String removeTags(String str) {
+        return str.substring(0, str.indexOf("fqdn"));
     }
 }
